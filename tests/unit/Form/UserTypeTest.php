b9becd853fce5144ef864008c04f14f1fc0f080b
DavidUMC <david.perez@upmycard.com>
2024-05-26 15:52:34 +0200

    added a few unit tests and improved test coverage

--- tests/unit/Form/UserTypeTest.php
+++ tests/unit/Form/UserTypeTest.php
@@ -1,32 +1,70 @@
 <?php
+
 namespace App\Tests\integration\Form;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Entity\Task;
+use App\Entity\User;
+use PHPUnit\Framework\TestCase;
+use Doctrine\Common\Collections\ArrayCollection;
 
-class UserTypeTest extends WebTestCase
+class UserTypeTest extends TestCase
 {
-    public function testSubmitForm(): void
+    public function testSetRolesWithValidRoleAdmin()
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/users/create');
-
-        $form = $crawler->selectButton('Ajouter')->form();
-        $form['user[email]'] = 'test@example.com';
-        $form['user[password][first]'] = 'password';
-        $form['user[password][second]'] = 'password';
-        $form['user[username]'] = 'Test';
-
-        $client->submit($form);
-
-        $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('div.alert-success', 'L\'utilisateur a bien été ajouté.');
+        // Create user and give him ROLE_ADMIN
+        $user = new User();
+        $user->setRoles(['ROLE_ADMIN']);
+        $this->assertEquals(['ROLE_ADMIN'], $user->getRoles());
     }
 
-    protected function tearDown(): void
+    public function testSetRolesWithValidRoleUser()
     {
-        $em = self::$kernel->getContainer()->get('doctrine')->getManager();
-        $em->getConnection()->executeQuery('DELETE FROM `task`');
-        $em->getConnection()->executeQuery('DELETE FROM `user`');
-        self::ensureKernelShutdown();
+        // Create a user and give him ROLE_USER
+        $user = new User();
+        $user->setRoles(['ROLE_USER']);
+        $this->assertEquals(['ROLE_USER'], $user->getRoles());
+    }
+
+    public function testAddTask()
+    {
+        $user = new User();
+        $task = new Task();
+
+        // Initial test : User has no task
+        $this->assertCount(0, $user->getTasks());
+
+        // Adds a task
+        $user->addTask($task);
+
+        // Check a task has been added
+        $this->assertCount(1, $user->getTasks());
+        $this->assertSame($task, $user->getTasks()[0]);
+        $this->assertSame($user, $task->getUser());
+    }
+
+    public function testAddTaskTwice()
+    {
+        $user = new User();
+        $task = new Task();
+
+        // Add a task
+        $user->addTask($task);
+
+        // Add  same task a second time
+        $user->addTask($task);
+
+        // Check task is not added twice
+        $this->assertCount(1, $user->getTasks());
+    }
+
+    public function testGetTasksAfterAddingTask()
+    {
+        $user = new User();
+        $task = new Task();
+        $user->addTask($task);
+
+        // Check if collection contains a task after adding one
+        $this->assertCount(1, $user->getTasks());
+        $this->assertSame($task, $user->getTasks()->first());
     }
 }
